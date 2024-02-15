640c5757b33cd8e360ed11f8e073cb9b4de29578
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-02-15 17:43:09 +0530

    users model

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,51 @@
-export const signup = (req, res) => {
-    res.send("signup")
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs'
+
+export const signup = async (req, res) => {
+  try{
+   const { fullName, username, password, confirmPassword, gender } = req.body;
+
+   if(password !== confirmPassword){
+    return res.status(400).json({error: "Password and confirm password don't match"});
+   }
+
+   const user = await User.findOne({username})
+
+   if(user){
+    return res.status(400).json({error: "Username already exists"});
+   }
+
+//  hash password 
+
+const salt = await bcrypt.genSalt(10);
+const hashedPassword = await bcrypt.hash(password, salt);
+ 
+  const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+  const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+ 
+  const newUser = new User({
+    fullName:fullName,
+    username:username,
+    password:hashedPassword,
+    gender:gender,
+    profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+  })
+
+  await newUser.save();
+
+  res.status(201).json({
+    _id:newUser._id,
+    fullName:newUser.fullName,
+    username:newUser.username,
+    password:newUser.password,
+    gender:newUser.gender,
+    profilePic:newUser.profilePic
+  })
+
+  } catch(error){
+    console.log("error in sign up controller",error.message)
+    res.status(500).json({error: "Internal server error"})
+  }
 }
 
 export const login = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes.js'
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
-dotenv.config();
- 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello world"); 
-})
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests 
 
 app.use("/api/auth", authRoutes)
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+// app.get("/", (req, res) => {
+//     res.send("Hello world"); 
+// })
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server Runing on Port ${PORT}`)
+})
