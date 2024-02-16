cc56d94b1e552ffa6e425a791211394f1d2b059d
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-02-16 14:53:35 +0530

    backend complete

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -35,7 +35,29 @@ export const sendMessage = async (req, res) => {
     res.status(201).json({ newMessage });
 
    } catch (error){
-    console.log("error in sendMessage", error.message);
+    console.log("error in sendMessage controller", error.message);
     res.status(500).json({ error: "Internal server error"});
    }
+}
+
+export const getMessage = async (req, res) => {
+    try{
+
+     const { id:userToChatId } = req.params;
+     const senderId = req.user._id;
+
+     const conversation = await Conversation.findOne({
+        participants: { $all: [senderId, userToChatId] },
+     }).populate("messages"); // actual messages
+
+     if(!conversation) return res.status(200).json([]);
+
+     const messages = conversation.messages;
+
+     res.status(200).json(messages);
+
+    } catch (error){
+    console.log("error in getMessage controller", error.message);
+    res.status(500).json({ error: "Internal server error"});
+    }
 }
