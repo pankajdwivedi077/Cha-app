6f0fc07686f1d3bca6d78d00364b63cdcdeb2c80
Pankaj Dwivedi <pankajdwivedi077@gmail.com>
2024-02-16 18:16:24 +0530

    Ui design complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
 
@@ -8,7 +9,8 @@ function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       {/* <Login /> */}
-      <SignUp />
+      {/* <SignUp /> */}
+      <Home />
     </div>
   )
 }
